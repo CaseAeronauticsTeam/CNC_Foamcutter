0cba33d645f4946def9face57d39ad4474808e12
mitchelw <57276362+mitchelw@users.noreply.github.com>
2022-09-03 13:45:50 -0400

    Two-Axis Test Complete

--- Firmware/teensy_41/src/main.cpp
+++ Firmware/teensy_41/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-const int xJoyPin = 23;
-const int yJoyPin = 22;
+const int xJoyPin = 22;
+const int yJoyPin = 23;
 const int xDirPin = 4;
 const int yDirPin = 5;
 const int xStepPin = 6;
@@ -16,8 +16,8 @@ void setup() {
 
 const int adc_bitwidth = 10;
 const int noise_margin = 50;
-const int max_delay = 1000;
-const int min_delay = 20;
+const int max_delay = 2000;
+const int min_delay = 100;
 const int pulse_width = 1;  // usec
 const double delay_coeff = 1;
 const int half_adc_max = 1 << (adc_bitwidth - 1);
@@ -48,18 +48,18 @@ void loop() {
     }
 
     if (y_joy > ((1 << (adc_bitwidth - 1)) + noise_margin)) {
-        digitalWrite(yDirPin, 1);
-        digitalWrite(yStepPin, 1);
-        delayMicroseconds(pulse_width);
-        digitalWrite(yStepPin, 0);
-        var_udelay = (double)(min_delay - max_delay) * (double)(x_joy - half_adc_max) / (double)half_adc_max + max_delay; // May have casting/truncation issues
-        delayMicroseconds((int)(var_udelay * delay_coeff));
-    } else if (y_joy < ((1 << (adc_bitwidth - 1)) - noise_margin)) {
         digitalWrite(yDirPin, 0);
         digitalWrite(yStepPin, 1);
         delayMicroseconds(pulse_width);
         digitalWrite(yStepPin, 0);
-        var_udelay = (double)(max_delay - min_delay) * (double)x_joy / (double)half_adc_max + min_delay;
+        var_udelay = (double)(min_delay - max_delay) * (double)(y_joy - half_adc_max) / (double)half_adc_max + max_delay; // May have casting/truncation issues
+        delayMicroseconds((int)(var_udelay * delay_coeff));
+    } else if (y_joy < ((1 << (adc_bitwidth - 1)) - noise_margin)) {
+        digitalWrite(yDirPin, 1);
+        digitalWrite(yStepPin, 1);
+        delayMicroseconds(pulse_width);
+        digitalWrite(yStepPin, 0);
+        var_udelay = (double)(max_delay - min_delay) * (double)y_joy / (double)half_adc_max + min_delay;
         delayMicroseconds((int)(var_udelay * delay_coeff));
     }
 }
