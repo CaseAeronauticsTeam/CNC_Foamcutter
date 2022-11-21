5bf776342ca9f28eb4d51ec4fd5f03f573c28697
Mitchel Walker <mitchelwalks@gmail.com>
2022-11-21 13:06:28 -0500

    Reorganized code to separate gcode_writer.py

--- Scripts/Gcode/2AxisGcodeFromDXF.py
+++ Scripts/Gcode/2AxisGcodeFromDXF.py
@@ -1,7 +1,9 @@
 import math
-
+import datetime
 import ezdxf
 import matplotlib.pyplot as plt
+from gcode_writer import generate_2_axis_gcode
+
 
 dx = 0.1
 
@@ -17,13 +19,16 @@ def plot(x_series, y_series, plot_name):
     fig, ax = plt.subplots()
     # ax.plot(x_series, y_series, "#ff0000", lw=.1)
     for i in range(1, len(x_series)):
+        x1, x2 = x_series[i-1:i+1]
+        y1, y2 = y_series[i-1:i+1]
         # ax.plot(x_series[i-1:i+1], y_series[i-1:i+1], f"#{'%06x' % (i // 2)}", lw=.1)
-        ax.plot(x_series[i-1:i+1], y_series[i-1:i+1], f"#ff0000", lw=.1)
-        print(f"Cords: {(x_series[i-1:i+1], y_series[i-1:i+1])}")
+        ax.plot((x1, x2), (y1, y2), f"#ff0000", lw=.2)
+        plt.arrow(x1, y1, x2 - x1, y2 - y1, shape='full', lw=0, length_includes_head=True, head_width=1)
+        # print(f"Cords: {((x1, x2), (y1, y2))}")
         # print(f"Color: #{'%06x' % (i // 8)}")
 
 
-    # plt.plot(x_series[0], y_series[0], marker="o", markersize=10, markeredgecolor="#00ff00", markerfacecolor="#00000000")
+    plt.plot(x_series[0], y_series[0], marker="o", markersize=10, markeredgecolor="#00ff00", markerfacecolor="#00000000")
 
     ax.set(xlabel='mm', ylabel='mm', title=plot_name)
 
@@ -48,13 +53,66 @@ class Shape:
         self.name = name
 
 
+def get_shapes_from_msp(msp):
+    shapes = []
+    for e in msp:
+        if e.dxftype() == "LINE":
+            print("LINE on layer: %s\n" % e.dxf.layer)
+            print("start point: %s\n" % e.dxf.start)
+            print("end point: %s\n" % e.dxf.end)
+            x1 = e.dxf.start[0] * 25.4
+            y1 = e.dxf.start[1] * 25.4
+            x2 = e.dxf.end[0] * 25.4
+            y2 = e.dxf.end[1] * 25.4
+            line = Shape([(x1, y1), (x2, y2)], "Line")
+            shapes.append(line)
+        elif e.dxftype() == "SPLINE":
+            print("SPLINE on layer: %s" % e.dxf.layer)
+            print(f"Degree {e.dxf.degree}")
+            print(f"Control points {e.dxf.n_control_points}")
+            print(f"Closed {e.closed}")
+            points = []
+            for cp in e.control_points:
+                points.append((cp[0] * 25.4, cp[1] * 25.4))
+            spline = Shape(points, "Spline")
+            shapes.append(spline)
+        elif e.dxftype() == "ARC":
+            print(f"Start Angle {e.dxf.start_angle}")
+            print(f"End Angle {e.dxf.end_angle}")
+            start_angle = e.dxf.start_angle
+            end_angle = e.dxf.end_angle
+            center = e.dxf.center
+            radius = e.dxf.radius
+            print(f"Radius: {radius}")
+            dth1 = math.degrees(2 * math.asin(dx / (2 * radius * 25.4)))
+            # Always moving counter-clockwise (positive theta)
+            if start_angle < end_angle:
+                theta = start_angle - end_angle
+            else:
+                theta = 360 - abs(start_angle - end_angle)
+            n_wedges = math.floor(theta / dth1)
+            dth2 = math.radians(theta / n_wedges)  # dth1= degrees, dth2 = radians
+            print(f"N:{n_wedges}, Theta:{theta}")
+            print(f"dth2 rad: {dth2}, dth2 deg: {math.degrees(dth2)}, dth1: {dth1}")
+            points = []
+            for i in range(n_wedges):
+                current_angle = i * dth2 + math.radians(start_angle)
+                # print(f"Current Angle: {current_angle}")
+                # print(f"x: {25.4 * radius * math.cos(current_angle) + center[0] * 25.4}")
+                points.append((25.4 * radius * math.cos(current_angle) + center[0] * 25.4,
+                               25.4 * radius * math.sin(current_angle) + center[1] * 25.4))
+            arc = Shape(points, "Arc")
+            shapes.append(arc)
+    return shapes
+
+
 def order_points_from_shapes(shapes):
     # shapes.pop(3)
     print(f"Candidate Shapes: {[f.name for f in shapes]}")
     print(f"Candidate S/E: {[(f.start, f.end) for f in shapes]}")
     largest_dist = 0
     largest_shape = None
-    for shape in shapes: # FInd the largest shape
+    for shape in shapes:  # Find the largest shape
         dist = 0
         for i in range(1, len(shape.points)):
             dist += math.dist(shape.points[0], shape.points[1])
@@ -100,7 +158,7 @@ def order_points_from_shapes(shapes):
     return points
 
 
-def main():
+def generate_gcode_from_dxf(input_filename, output_filename, show_plot=True, reverse=False):
     try:
         doc = ezdxf.readfile("D:\\Projects\\CaseAeronauticsTeam\\CNC_Foamcutter\\Airfoils\\NACA2412 with Hole.DXF")
     except IOError:
@@ -110,73 +168,43 @@ def main():
         print(f"Invalid or corrupted DXF file.")
         exit()
 
-    # iterate over all entities in modelspace
     msp = doc.modelspace()
 
-    shapes = []
-
+    print("DXF Contents:")
     for e in msp:
-        if e.dxftype() == "LINE":
-            print("LINE on layer: %s\n" % e.dxf.layer)
-            print("start point: %s\n" % e.dxf.start)
-            print("end point: %s\n" % e.dxf.end)
-            x1 = e.dxf.start[0] * 25.4
-            y1 = e.dxf.start[1] * 25.4
-            x2 = e.dxf.end[0] * 25.4
-            y2 = e.dxf.end[1] * 25.4
-            line = Shape([(x1, y1), (x2, y2)], "Line")
-            shapes.append(line)
-        elif e.dxftype() == "SPLINE":
-            print("SPLINE on layer: %s" % e.dxf.layer)
-            print(f"Degree {e.dxf.degree}")
-            print(f"Control points {e.dxf.n_control_points}")
-            print(f"Closed {e.closed}")
-            points = []
-            for cp in e.control_points:
-                points.append((cp[0] * 25.4, cp[1] * 25.4))
-            spline = Shape(points, "Spline")
-            shapes.append(spline)
-        elif e.dxftype() == "ARC":
-            print(f"Start Angle {e.dxf.start_angle}")
-            print(f"End Angle {e.dxf.end_angle}")
-            start_angle = e.dxf.start_angle
-            end_angle = e.dxf.end_angle
-            center = e.dxf.center
-            radius = e.dxf.radius
-            dth1 = math.degrees(2 * math.asin(dx / (2 * radius * 25.4)))
-            # Always moving counter-clockwise (positive theta)
-            if start_angle < end_angle:
-                theta = start_angle - end_angle
-            else:
-                theta = 360 - abs(start_angle - end_angle)
-            n_wedges = math.floor(theta / dth1)
-            dth2 = math.radians(theta / n_wedges)  # dth1= degrees, dth2 = radians
-            print(f"N:{n_wedges}, Theta:{theta}")
-            print(f"dth2 rad: {dth2}, dth2 deg: {math.degrees(dth2)}, dth1: {dth1}")
-            points = []
-            for i in range(n_wedges):
-                current_angle = i * dth2 + math.radians(start_angle)
-                print(f"Current Angle: {current_angle}")
-                print(f"x: {25.4 * radius * math.cos(current_angle) + center[0] * 25.4}")
-                points.append((25.4 * radius * math.cos(current_angle) + center[0] * 25.4,
-                               25.4 * radius * math.sin(current_angle) + center[1] * 25.4))
-            arc = Shape(points, "Arc")
-            shapes.append(arc)
+        print(e.dxftype())
+    print("\n\n")
+
+    shapes = get_shapes_from_msp(msp)
 
     ordered_points = order_points_from_shapes(shapes)
-    # ordered_points = shapes[0].points
+
+    # Find left most point and rotate list to make that the starting point
+    min_x = 9e99
+    start_index = 0
+    for i, p in enumerate(ordered_points):
+        if p[0] < min_x:
+            min_x = p[0]
+            start_index = i
+    print(f"Min_x: {min_x}, Start Index: {start_index}")
+    ordered_points = ordered_points[start_index:] + ordered_points[:start_index]
+
+    if reverse:
+        ordered_points.reverse()
+
+    # Check to make sure points list is a closed loop
+    if ordered_points[0] != ordered_points[-1]:
+        ordered_points.append(ordered_points[0])
+
     x = [p[0] for p in ordered_points]
     y = [p[1] for p in ordered_points]
 
-    plot(x, y, "Test")
+    if show_plot:
+        plot(x, y, "Test")
 
-    print("==========================")
-    for e in msp:
-        print(e.dxftype())
-    # entity query for all LINE entities in modelspace
-    # for e in msp.query("LINE"):
-    #     print_entity(e)
+    generate_2_axis_gcode(x, y, input_filename, output_filename)
 
 
 if __name__ == '__main__':
-    main()
+    generate_gcode_from_dxf("D:\\Projects\\CaseAeronauticsTeam\\CNC_Foamcutter\\Airfoils\\NACA2412 with Hole.DXF",
+                            "out.gcode", show_plot=True)
---
+++ Scripts/Gcode/gcode_writer.py
@@ -0,0 +1,28 @@
+import math
+import datetime
+
+
+def generate_2_axis_gcode(x_series, y_series, input_filename, output_filename, speed=5):
+    assert len(x_series) == len(y_series)
+    out = ""
+    length = 0
+    last_x, last_y = x_series[0], y_series[0]
+
+    # Normalize the G-Code to the first point in the CSV
+    x_origin_offset = x_series[0]
+    y_origin_offset = y_series[0]
+
+    for x, y in zip(x_series, y_series):
+        out += f"G1 X{x - x_origin_offset} Y{y - y_origin_offset}\n"
+        length += math.sqrt((x - last_x) ** 2 + (y - last_y) ** 2)
+        last_x, last_y = x, y
+    print(f"Total Length of cut: {length}mm   ==>   ~{length / (speed * 60)} minutes @ 5mm/s")
+
+    print(f"Writing G-Code to {output_filename}")
+    f = open(output_filename, "w")
+    f.write(";FLAVOR:Marlin\n;For: CAT 2-Axis CNC Foamcutter\n")
+    f.write(f";Created: {datetime.datetime.now()}\n")
+    f.write(f";From: \"{input_filename}\"\n\n")
+    f.write(out)
+    f.close()
+    print(f"Successfully written {output_filename}")
